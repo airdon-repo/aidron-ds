59ce7e8a7a395b628455de3be83c424a47b50166
Fernando Camilotti <fernando.camilotti@domo.com>
2024-01-11 18:23:30 -0300

    upgraded storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,26 @@
-const path = require('path');
-
+const path = require("path");
 module.exports = {
-  "stories": [
+  stories: [
     "../src/stories/Introduction.stories.mdx",
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+
+  addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "storybook-dark-mode"
+    "@storybook/preset-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        sass: { implementation: require("sass") },
+      },
+    },
+    // "storybook-dark-mode",
+    // "@storybook/addon-mdx-gfm"
   ],
+
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
@@ -21,11 +29,22 @@ module.exports = {
     // Make whatever fine-grained changes you need
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
     });
 
     // Return the altered config
     return config;
   },
-}
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {
+    autodocs: true,
+  },
+};
